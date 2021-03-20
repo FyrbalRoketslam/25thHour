0d83872aee5495a35827068bc23ad5958a0a07b4
HuckRob <RobertBuentgen@u.boisestate.edu>
2021-03-20 15:24:05 -0600

    Card Game Update

--- game.js
+++ game.js
@@ -1,9 +1,23 @@
+//To Do
+//Make the players cards not take up space when they are not visible
+//  - display: none; ???
+
 var playerHand = "";
 
-//Start Of game select deck
+//Deck Select Buttons
 var swordBtn = document.getElementById('sword-btn');
 var wandBtn  = document.getElementById('wand-btn');
 var cupBtn   = document.getElementById('cup-btn');
+
+//View Cards After Selecting Deck Buttons
+var viewSwordBtn = document.getElementById('player-sword-cards');
+var viewWandBtn  = document.getElementById('player-wand-cards');
+var viewCupBtn   = document.getElementById('player-cup-cards');
+
+//Buttons for player to select card afte they select deck and its in the center
+var leftCardBtn = document.getElementById('player-sword-cards');
+var middleCardBtn  = document.getElementById('player-wand-cards');
+var rightCardBtn   = document.getElementById('player-cup-cards');
 
 //When the player selects the sword deck
 swordBtn.onclick = function(){
@@ -50,7 +64,7 @@ wandBtn.onclick = function(){
         //Shows the enemys card
         enemyDeck.style.visibility = "visible";
         enemyDeck.style.display = "block";
-        layerHand = "wand";
+        playerHand = "wand";
     }
 }
 
@@ -74,7 +88,101 @@ cupBtn.onclick = function(){
         //Shows the enemys card
         enemyDeck.style.visibility = "visible";
         enemyDeck.style.display = "block";
-        layerHand = "cup";
+        playerHand = "cup";
     }
 }
 
+
+//When player has sword cards and wants to view then
+viewSwordBtn.onclick = function(){
+    var playersDeck = document.getElementsByClassName("player-card-deck-center")
+    var playersCards = document.getElementsByClassName("player-hand-photo");
+
+    for(var i = 0 ; i < 1; i++)
+    {
+    playersDeck[i].style.visibility = "visible";
+    }
+
+    playersCards[0].src = "Images/ConstarotCards/AndromedaCard.png";
+    playersCards[1].src = "Images/ConstarotCards/AndromedaCard.png";
+    playersCards[2].src = "Images/ConstarotCards/AndromedaCard.png";
+    
+}
+
+//Wand Button --- Players Hand
+viewWandBtn.onclick = function(){
+    var playersDeck = document.getElementsByClassName("player-card-deck-center")
+    var playersCards = document.getElementsByClassName("player-hand-photo");
+    for(var i = 0 ; i < 1; i++)
+    {
+    playersDeck[i].style.visibility = "visible";
+    }
+    playersCards[0].src = "Images/ConstarotCards/AndromedaCard.png";
+    playersCards[1].src = "Images/ConstarotCards/AndromedaCard.png";
+    playersCards[2].src = "Images/ConstarotCards/AndromedaCard.png";
+}
+
+
+//Cup Button ---- Playters Hand
+viewCupBtn.onclick = function(){
+    var playersDeck = document.getElementsByClassName("player-card-deck-center")
+    var playersCards = document.getElementsByClassName("player-hand-photo");
+
+    for(var i = 0 ; i < 1; i++)
+    {
+    playersDeck[i].style.visibility = "visible";
+    }
+    playersCards[0].src = "Images/ConstarotCards/AndromedaCard.png";
+    playersCards[1].src = "Images/ConstarotCards/AndromedaCard.png";
+    playersCards[2].src = "Images/ConstarotCards/AndromedaCard.png";
+}
+
+
+
+//Player selects left card
+leftCardBtn.onclick = function(){
+    switch(playerHand){
+        case "sword":
+
+            break;
+        case "wand":
+            
+
+            break;
+        case "cup":
+
+            break;
+    }
+}
+
+//Player selects middle card
+middleCardBtn.onclick = function(){
+    switch(playerHand){
+        case "sword":
+
+            break;
+        case "wand":
+            
+
+            break;
+        case "cup":
+
+            break;
+    }
+}
+
+//Player selects right card
+rightCardBtn.onclick = function(){
+    switch(playerHand){
+        case "sword":
+
+            break;
+        case "wand":
+            
+
+            break;
+        case "cup":
+
+            break;
+    }
+}
