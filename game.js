ce74ff712e3cb8b08f5b6f116905f7090a14734f
Katherine <73628642+FyrbalRoketslam@users.noreply.github.com>
2021-03-26 13:22:42 -0600

    added the rest of the fortunes

--- game.js
+++ game.js
@@ -307,49 +307,185 @@ function FinalReading(card1, card2) {
 
     switch (cardCombos) {
         case "OrionOrion":
-            console.log("Orion is endlessly chased by the scorpion and endlessly chases the seven sisters. He, like you, is a devoted hunter. He, unlike you, is also hunted. You may feel like you are constantly on the run, but take a breath and center yourself. Use your strength for your craft, not running from perceived threats. Should they come, you can take them.");
-            return "Orion is endlessly chased by the scorpion and endlessly chases the seven sisters. He, like you, is a devoted hunter. He, unlike you, is also hunted. You may feel like you are constantly on the run, but take a breath and center yourself. Use your strength for your craft, not running from perceived threats. Should they come, you can take them.";
+            return("Orion is endlessly chased by the scorpion and endlessly chases the seven sisters. He, like you, is a devoted hunter. He, unlike you, is also hunted. You may feel like you are constantly on the run, but take a breath and center yourself. Use your strength for your craft, not running from perceived threats. Should they come, you can take them.");
             break;
+
+            case "OrionDraco" || "DracoOrion":
+                return("Orion is endlessly chased by the scorpion and endlessly chases the seven sisters. Draco is a powerful dragon defeated. Although it might seem like people around you are defeated, and that same hunter is behind you, keep running, with your goals in sight. Have faith in your ability to push onward.");
+                break;
+
+                case "OrionAndromeda" || "AndromedaOrion":
+                    return("You might feel chained by a destiny that others have put on you, a sacrifice to the whim of the earth. However, showing your dedication and willpower will bring the right people in your life to show you your true path.");
+                    break;
+
+                    case "OrionOphiucus" || "OphiuchusOrion":
+                        return("With Orion’s powerful will and determination and Ophiuchus's power that even gods fear, be careful with the amount of force within you. Be cautious because you have a lot of potential for good, make sure to use it for such.");
+                        break;
+
+                        case "OrionVedrfolnir" || "VedrfolnirOrion":
+                            return("You have an ability to oversee what is around you, picking apart the rhetorics of wisdom and knowledge. But sometimes you get caught in the chase and forget to keep an eye. Don't let the thrill of hunt cloud your judgement. Keep fighting on, but recognize your keen eye.");
+                            break;
+
+                            case "OrionByakko" || "ByakkoOrion":
+                                return("While you are always keeping your sights on accomplishing goals and furious for success, fear of failure, you must remember to take a breath, recognize the times to sit and find bliss.");
+                                break;
+
+                                case "OrionCoronaBorealis" || "CoronaBorealisOrion":
+                                    return("Although you might be alone in your thrill of the hunt, recognize the gifts you have been given, the powerful connections. Appreciate the people that come into your life bearing hidden gifts.");
+                                    break;
+
+                                    case "OrionCygnus" || "CygusOrion":
+                                        return("You might be losing your true self in the pursuit of ever expansive goals. Recognize when to let go, or you might just lose yourself forever.");
+                                        break;
+
+                                        case "OrionLyra" || "LyraOrion":
+                                            return("Don't let your constant fear of someone leaving you push them away. Your battles might have sacrifices, that is the natural fight that we all fight, trust in the process and don't look back.");
+                                            break;
 
         case "DracoDraco":
-            console.log("A dragon, a god-fighter. Endlessly powerful. Be careful no to over do it. In your haste to fight, you will only end up hurting yourself and others. Allow yourself to be peaceful.");
-            return "A dragon, a god-fighter. Endlessly powerful. Be careful no to over do it. In your haste to fight, you will only end up hurting yourself and others. Allow yourself to be peaceful.";
+            return("A dragon, a god-fighter. Endlessly powerful. Be careful no to over do it. In your haste to fight, you will only end up hurting yourself and others. Allow yourself to be peaceful.");
             break;
+
+            case "DracoAndromeda" || "AndromedaDraco":
+                return("While you are always keeping your sights on accomplishing goals and furious for success, fear of failure, you must remember to take a breath, recognize the times to sit and find bliss.");
+                break;
+
+                case "DracoOphiuchus" || "OphiuchusDraco":
+                    return("You are strong, and though there have been many battles that have torn you down. Recoup and let yourself heal. You will emerge stronger than you have seen in yourself before.");
+                    break;
+
+                    case "DracoVedrfolnir" || "VedrfolnirDraco":
+                        return("You have a powerful perspective on things, but sometimes you get caught up in your own world allowing yourself to be blindsided. Realize your strength but also realize the power that others around you have.");
+                        break;
+
+                        case "DracoByakko" || "ByakkoDraco":
+                            return("Although you feel defeated, that just might be what it seems on the surface. This could be a pivotal moment for you. Make peace with what drags you down. Realize the importance of the moment you are going through.");
+                            break;
+
+                            case "DracoCoronaBorealis" || "CoronaBorealisDraco":
+                                return("Feeling struck down by the weight of it all, realize the love that might come your way. Some will break off spiteful, hurting, but another will come along and mend.");
+                                break;
+
+                                case "DracoCygnus" || "CygnusDraco":
+                                    return("Be careful of how strong your thoughts are and how they change your perspective. If you aren't aware, you might just manifest your own defeat.");
+                                    break;
+
+                                    case "DracoLyra" || "LyraDraco":
+                                        return("Don't doubt your own strength or abilities. Such doubt might cause your own downfall.");
+                                        break;
 
         case "AndromedaAndromeda":
-            console.log("Andromeda, constantly under threat of death for her parent's transgressions. You may feel powerless in the world around you. You must remember you are strong. Focus on what you can do-- not what you can't.");
-            return "Andromeda, constantly under threat of death for her parent's transgressions. You may feel powerless in the world around you. You must remember you are strong. Focus on what you can do-- not what you can't.";
+            return("Andromeda, constantly under threat of death for her parent's transgressions. You may feel powerless in the world around you. You must remember you are strong. Focus on what you can do-- not what you can't.");
             break;
 
+            case "AndromedaOphiuchus" || "OphiuchusAndromeda":
+                return("You are trying so hard to heal others because of how bright your light is. Sometimes you need to take time to nurture your own, or else you will sacrifice that light to someone else who only takes.");
+                break;
+
+                case "AndromedaVedrfolnir" || "VedrfolnirAndromeda":
+                    return("You can oversee the monsters that lay in the uncharted waters. Don't let other peoples’ fear consume you, think it through yourself. Don't let someone else define your destiny.");
+                    break;
+
+                    case "AndromedaByakko" || "ByakkoAndromeda":
+                        return("Although you might be stuck, awaiting the monster, it could be a blessing in disguise. Try to find peace in a dire situation.");
+                        break;
+
+                        case "AndromedaCoronaBorealis" || "CoronaBorealisAndromeda":
+                            return("Although you may feel trapped and alone, abandoned by your past lovers, look onward for someone who will see your true blessings.");
+                            break;
+
+                            case "AndromedaCygnus" || "CygnusAndromeda":
+                                return("You might be in a constant search for someone to save you from your situation. Sometimes you have to stop getting in your own way and allow yourself some peace.");
+                                break;
+
+                                case "AndromedaLyra" || "LyraAndromeda":
+                                    return("Stop depending on other people, do it yourself. Have trust in yourself and the others around you to see it.");
+                                    break;
+                                                                                    
         case "OphiuchusOphiuchus":
-            console.log("Ophiuchus was a legendary healer-- able, even, to cure death itself. He was killed to prevent immortality in humans. You have many healing gifts as well. Continue to help others selflessly and you will be rewarded.");
-            return "Ophiuchus was a legendary healer-- able, even, to cure death itself. He was killed to prevent immortality in humans. You have many healing gifts as well. Continue to help others selflessly and you will be rewarded.";
+            return("Ophiuchus was a legendary healer-- able, even, to cure death itself. He was killed to prevent immortality in humans. You have many healing gifts as well. Continue to help others selflessly and you will be rewarded.");
             break;
+
+            case "OphiuchusVedrfolnir" || "VedrfolnirOphiuchus":
+                return("You can oversee and plan what is to come. Life is coming together piece by piece. Use your undeniable power, don't be afraid. It is all lining up.");
+                break;
+    
+                case "OphiuchusByakko" || "ByakkoOphiuchus":
+                    return("You are wiser than you think, use the times of peace to heckin heal yourself bro.");
+                    break;
+    
+                    case "OphiuchusCoronaBorealis" || "CoronaBorealisOphiuchus":
+                        return("Your healing power will be recognized by someone great who is on the way. Await the gifts to come.");
+                        break;
+    
+                        case "OphiuchusCygnus" || "CygnusOphiuchus":
+                            return("Although you have a want to heal everyone, you must recognize at times that other peoples’ problems are their own.");
+                            break;
+    
+                            case "OphiuchusLyra" || "LyraOphiuchus":
+                                return("Don't doubt your healing abilities, others around you appreciate it. If you seek constant reassurance about your own power, others might see you as selfish and be taken away.");
+                                break;
+                                            
 
         case "VedrfolnirVedrfolnir":
-            console.log("Vedrfolnir, perched atop an eagle in the World Tree, surveys all and knows just as much. Use your vast knowledge for good. Share it with others to create a better world.");
-            return "Vedrfolnir, perched atop an eagle in the World Tree, surveys all and knows just as much. Use your vast knowledge for good. Share it with others to create a better world.";
+            return("Vedrfolnir, perched atop an eagle in the World Tree, surveys all and knows just as much. Use your vast knowledge for good. Share it with others to create a better world.");
             break;
+
+            case "VedrfolnirByakko" || "ByakkoVedrfolnir":
+                return("Wisdom, peace, virtue, and clear sight. Meditate on those concepts, how they resonate with you, in order to find your own piece, you have the knowledge to do it.");
+                break;
+    
+                case "VedrfolnirCoronaBorealis" || "CoronaBorealisVedrfolnir":
+                    return("Don't overlook the gifts that manifest around you. Even if you are lost on an island, in a constant search for what is good. It might just lie at your feet.");
+                    break;
+    
+                    case "VedrfolnirCygnus" || "CygnusVedrfolnir":
+                        return("You might have planned something that is un achievable at this moment, don't focus too hard on it. Let it motivate you, but don't let it get you down.");
+                        break;
+    
+                        case "VedrfolnirLyra" || "LyraVedrfolnir":
+                            return("You might be confident in seeing things, but also have faith. Seeing with your physical is powerful, but don't forget the faith you must keep inside.");
+                            break;
+                            
 
         case "ByakkoByakko":
-            console.log("Byakko, a white tiger, is said to show up in in times of peace or when the emperor in truly virtuous. These may not be true, but they may be within yourself. Share your inner peace and virtue with others, and the world may follow suit.");
-            return "Byakko, a white tiger, is said to show up in in times of peace or when the emperor in truly virtuous. These may not be true, but they may be within yourself. Share your inner peace and virtue with others, and the world may follow suit.";
+            return("Byakko, a white tiger, is said to show up in in times of peace or when the emperor in truly virtuous. These may not be true, but they may be within yourself. Share your inner peace and virtue with others, and the world may follow suit.");
             break;
+            
+            case "ByakkoCoronaBorealis" || "CoronaBorealisByakko":
+                return("Even if you are on an island, you can still relish in the bliss and peace that you create within yourself. Others will come, don't worry about them, finding it within yourself might bring them.");
+                break;
+    
+                case "ByakkoCygnus" || "CygnusByakko":
+                    return("Life can be one big comedic tragedy, we all can relate at some point. The one thing that pushes us all through, the one thing that makes humanity beautiful, is hope.");
+                    break;
+    
+                    case "ByakkoLyra" || "LyraByakko":
+                        return("Don't doubt the virtues of others and have trust that they will be around.");
+                        break;
+                
 
         case "CoronaBorealisCoronaBorealis":
-            console.log("Immortalizing Dionysis' love for Ariadne after she was abandoned, the Northern crown adornes the sky. You may have experienced a sad departure from your life, but know there is a light at the end if you just keep going.");
-            return "Immortalizing Dionysis' love for Ariadne after she was abandoned, the Northern crown adornes the sky. You may have experienced a sad departure from your life, but know there is a light at the end if you just keep going.";
+            return("Immortalizing Dionysis' love for Ariadne after she was abandoned, the Northern crown adornes the sky. You may have experienced a sad departure from your life, but know there is a light at the end if you just keep going.");
             break;
+            
+            case "CoronaBorealisCygnus" || "CygnusCoronaBorealis":
+                return("It may suck, but just wait a while and it will get better. Time will heal. You will get through this, I believe in you. <3");
+                break;
+    
+                case "CoronaBorealisLyra" || "LyraCoronaBorealis":
+                    return("If you are stuck in doubt, you won't see the gifts that come. If you worry constantly about purpose and faith, you might just miss out on the present moment.");
+                    break;
+        
 
         case "CygnusCygnus":
-            console.log("Cygnus was immortalized in the stars for his devotion to finding Icarus after his fall. Like Cygnus, you are searching for a lost love you may or may not have ever known. You search tirelessly, but it will get you nowhere if you disregard yourself. Take time to breath and mourn.");
-            return "Cygnus was immortalized in the stars for his devotion to finding Icarus after his fall. Like Cygnus, you are searching for a lost love you may or may not have ever known. You search tirelessly, but it will get you nowhere if you disregard yourself. Take time to breath and mourn.";
+            return("Cygnus was immortalized in the stars for his devotion to finding Icarus after his fall. Like Cygnus, you are searching for a lost love you may or may not have ever known. You search tirelessly, but it will get you nowhere if you disregard yourself. Take time to breath and mourn.");
             break;
 
-        case "LyraLyra":
-            console.log("Orpheus' lyre was crucial to him being able to save his wife from the underworld. He tossed it away when his own doubt caused his attempt to fail. You may have many trials ahead of you, but try and trust those around you. If someone reaches out their hand to help, trust their intentions.");
-            return "Orpheus' lyre was crucial to him being able to save his wife from the underworld. He tossed it away when his own doubt caused his attempt to fail. You may have many trials ahead of you, but try and trust those around you. If someone reaches out their hand to help, trust their intentions.";
-            break;
+            case "CygnusLyra" || "LyraCygnus":
+                return("You may be in an awful situation. Let yourself mourn for what was, focusing on what could be.");
+                break;
+
         default:
             return "Default Output";
             break;
